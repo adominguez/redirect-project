701682e471476654df5be1a01be8eb536e9fe867
ADominguez <dmartin.alberto@gmail.com>
2020-05-17 11:26:29 +0200

    fix(routes): add new escaneres-3d route

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,12 @@ exports.routes = (app) => {
     app.get('/impresoras-3d', function(request, response) {
         response.redirect(301, 'https://3dmakernow.com/comprar-tu-primera-impresora-3d/')
     });
+    /**
+     * Cuando monte la parte de los escaneres, hay que realizar la redirección de esta parte
+     */
+    app.get('/escaneres-3d', function(request, response) {
+        response.redirect(301, 'https://3dmakernow.com/')
+    });
     app.get('*', function(request, response) {
         response.redirect(301, 'https://3dmakernow.com')
     });
